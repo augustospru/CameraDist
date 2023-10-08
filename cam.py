aad1608fe6faaaadf0c252ee5d9d09071aebaf56
augusto <augusto_prudencio@hotmail.com>
2023-10-08 12:43:07 -0300

    get dist

--- cam.py
+++ cam.py
@@ -8,28 +8,15 @@ s = serial.Serial("COM3", 11520)
 FRAME_HEAD = b"\x00\xFF"
 FRAME_TAIL = b"\xDD"
 
-
 s.write(b"AT+FPS=10\r") #seta FPS para 10 
 time.sleep(0.1)
-s.write(b"AT+DISP=3\r") #seta para leitura de usb, disp=1 seta pra UART 
-
+s.write(b"AT+DISP=3\r") #seta para leitura de usb e display no lcd, disp=1 seta pra UART 
 time.sleep(0.1)
 s.flush() #flush buffer
 
-getHeader = False
-rawData = b''
 while s.is_open:
     if not s.readable():
         continue
-
-    # if not getHeader:
-    #     rawData += s.read()
-    #     idx = rawData.find(FRAME_HEAD)
-    #     if idx < 0:
-    #         continue
-    #     # print(rawData)
-    #     getHeader = True
-    #     rawData = rawData[idx:]
 
     packet = s.read(10022)
     idxHead = packet.find(FRAME_HEAD)
@@ -39,14 +26,15 @@ while s.is_open:
     packet += s.read(10022 - len(packet))
 
     img = packet[20:10020]
-
+    # print(packet[0:2].hex(' '), packet[10020:10022].hex(' '))
+    dist = ((img[4949]/5.1)**2)/10 #distance in cm, If UNIT is 0，the distance between this pixel and top is (p/5.1)^2
+    print(img[4949], dist)
 
     byteArray = bytearray()
     byteArray.extend(img)
     
     flatNumpyArray = np.array(byteArray) #convert byte array to numpyarray
-    # Convert the array to make a 100x100 grayscale image.
-    grayImage = flatNumpyArray.reshape(100,100)
+    grayImage = flatNumpyArray.reshape(100,100) # Convert the array to make a 100x100 grayscale image.
     grayImageResized = cv2.resize(grayImage, (500,500)) #resize image
     cv2.imshow('frame', grayImageResized)
     if cv2.waitKey(1) == ord('q'):
