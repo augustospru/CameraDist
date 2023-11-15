584788e0e39403a146adc062e4da0d0adfefa6e6
augusto <augusto_prudencio@hotmail.com>
2023-11-15 16:23:58 -0300

    Final project rasp

--- cam.py
+++ cam.py
@@ -2,8 +2,28 @@ import serial
 import cv2
 import numpy as np
 import time
+import threading
+import RPi.GPIO as GPIO
+import os
 
-s = serial.Serial("COM3", 115200)
+def dist(value: int):
+    return ((value/5.1)**2)/10 #distance in cm, If UNIT is 0，the distance between this pixel and top is (p/5.1)^2
+
+def vibration_main(dist: float, idx: int): #main vibration controller
+    if dist < 150:
+        idx_relative = idx % 25 # module by frame to get every line
+        if idx_relative < 8: 
+            GPIO.output(GPIO_1, True) # vibrates left motor
+        elif idx_relative < 16: 
+            GPIO.output(GPIO_2, True) # vibrates middle motor
+        elif idx_relative < 25: 
+            GPIO.output(GPIO_3, True) # vibrates right motor
+            
+def unpack(packet: list[int]): #function to unpack frame packet and send to vibrate
+    for idx, value in enumerate(packet):
+        vibration_main(dist(value), idx)
+
+s = serial.Serial("/dev/ttyUSB0", 115200) # init serial for communication
 
 FRAME_HEAD = b"\x00\xFF"
 FRAME_TAIL = b"\xDD"
@@ -12,12 +32,21 @@ s.write(b"AT+FPS=10\r") #seta FPS para 10
 time.sleep(0.1)
 s.write(b"AT+DISP=3\r") #seta para leitura de usb e display no lcd, disp=1 seta pra UART 
 time.sleep(0.1)
-s.write(b"AT+BINN=4\r") #seta frame para 25x25 
+s.write(b"AT+BINN=4\r") #seta frame para 25x25
 time.sleep(0.1)
 s.flush() #flush buffer
 
-def dist(value: int):
-    return ((value/5.1)**2)/10 #distance in cm, If UNIT is 0，the distance between this pixel and top is (p/5.1)^2
+# Define GPIO to use on Pi
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO_1 = 4
+GPIO_2 = 3
+GPIO_3 = 2
+
+#define GPIO as outputs
+GPIO.setup(GPIO_1, GPIO.OUT)
+GPIO.setup(GPIO_2, GPIO.OUT)
+GPIO.setup(GPIO_3, GPIO.OUT)
 
 while s.is_open:
     if not s.readable():
@@ -27,28 +56,15 @@ while s.is_open:
     idxHead = packet.find(FRAME_HEAD) #locate frame head
     if idxHead < 0:
             continue
+    
     packet = packet [idxHead:] #get packet from frame head
     packet += s.read(647 - len(packet)) #get the rest of the frame
-
-    img = packet[20:645] #get the image
-    # print(packet[0:2].hex(' '), packet[10020:10022].hex(' '))
-    # distpixel = dist(img[4949])
-    # distimg = ([dist(img[i]) for i in range(10000)]) #get the distance for each pixel
-    # print(img[4949], distpixel, distimg[4949])
-    # for idx, item in enumerate(img):
-    #     if dist(item) < 100:
-    #         if idx % 100 < 33: print("esquerda")
-    #         elif idx % 100 < 66: print("meio")
-    #         elif idx % 100 < 100: print("direita")
-
-    byteArray = bytearray()
-    byteArray.extend(img)
     
-    flatNumpyArray = np.array(byteArray) #convert byte array to numpyarray
-    grayImage = flatNumpyArray.reshape(25,25) # Convert the array to make a 100x100 grayscale image.
-    grayImageResized = cv2.resize(grayImage, (500,500)) #resize image
-    cv2.imshow('frame', grayImageResized)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    #initiate vibrate motors with false
+    GPIO.output(GPIO_1, False)
+    GPIO.output(GPIO_2, False)
+    GPIO.output(GPIO_3, False)
 
-cv2.destroyAllWindows()
+    #instanciate threads
+    vibration_thread = threading.Thread(target=unpack, args=(packet[20:645],))
+    vibration_thread.run()
